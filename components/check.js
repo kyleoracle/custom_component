f885d8a466cfa52063e6c8f744b2e7bf990fabfb
kyleoracle <kyle.z.zhang@oracle.com>
2019-10-14 13:16:40 +0800

    extract app name, and change ssh to http to check version

--- components/check.js
+++ components/check.js
@@ -1,37 +1,66 @@
 'use strict';
-var SSH = require('simple-ssh');
-var path = require('path');
+// var SSH = require('simple-ssh');
+var request = require('request');
+
 
 module.exports = {
   metadata: () => ({
     name: 'check',
     timeout : 1000000,
     properties: {
-      ip: { required: true, type: 'string' },
-      key: { required: true, type: 'string' }
+      appName: { required: true, type: 'string' },
+      url: { required: true, type: 'string' }
     }
   }),
   invoke: (conversation, done) => {
 
-    const { ip } = conversation.properties();
-    const { key } = conversation.properties();
+    const { appName } = conversation.properties();
+    const { url } = conversation.properties();
 
-    // ssh
-    var ssh = new SSH({
-      host: ip,
-      user: 'opc',
-      key: key
-    });
-    ssh.exec('kubectl get deployments/myapp', {
-      out: function(stdout) {
-          console.log(stdout);
+    if(appName && url){
+      request.get(url, function (error, response, body) {
+          if(error){
+              console.log(error);
+          }
+          console.log('response--------');
+          console.log(JSON.stringify(response));
+          console.log('body--------');
+          console.log(JSON.stringify(body));
+          console.log('error--------');
+          console.log(JSON.stringify(error));
+
           // reply
           conversation
-          .reply(stdout)
+          .reply(`status of ${appName} is ${body}`)
           .transition();
           done();
-      }
-    }).start();
+      });
+
+    } else {
+         // reply
+         conversation
+         .reply(`wrong param, app name=${appName} and url=${url}`)
+         .transition();
+         done();
+    }
+
+    
+    // ssh
+    // var ssh = new SSH({
+    //   host: ip,
+    //   user: 'opc',
+    //   key: key
+    // });
+    // ssh.exec('kubectl get deployments/myapp', {
+    //   out: function(stdout) {
+    //       console.log(stdout);
+    //       // reply
+    //       conversation
+    //       .reply(stdout)
+    //       .transition();
+    //       done();
+    //   }
+    // }).start();
 
   }
 };
--- components/deploy.js
+++ components/deploy.js
@@ -5,6 +5,7 @@ module.exports = {
   metadata: () => ({
     name: 'deploy',
     properties: {
+      appName: { required: true, type: 'string' },
       version: { required: true, type: 'string' },
       werckerToken: { required: true, type: 'string' },
       pipelineIdV1: { required: true, type: 'string' },
@@ -13,12 +14,13 @@ module.exports = {
   }),
   invoke: (conversation, done) => {
     // perform conversation tasks.
+    const { appName } = conversation.properties();
     const { version } = conversation.properties();
     const { werckerToken } = conversation.properties();
     const { pipelineIdV1 } = conversation.properties();
     const { pipelineIdV2 } = conversation.properties();
 
-
+    conversation.logger().info("appName=" + appName);
     conversation.logger().info("version=" + version);
 
     // call wercker
@@ -34,30 +36,37 @@ module.exports = {
         body.pipelineId = pipelineIdV2;
     }
 
-    const options = {
-        url: 'https://app.wercker.com/api/v3/runs',
-        json: true,
-        headers: headers,
-        body: body
-    };
+    if(appName && version){
+      const options = {
+          url: 'https://app.wercker.com/api/v3/runs',
+          json: true,
+          headers: headers,
+          body: body
+      };
 
-    request.post(options, function (error, response, body) {
-        if(error){
-            console.log(error);
-        }
-        console.log('response--------');
-        console.log(JSON.stringify(response));
-        console.log('body--------');
-        console.log(JSON.stringify(body));
-        console.log('error--------');
-        console.log(JSON.stringify(error));
-    });
+      request.post(options, function (error, response, body) {
+          if(error){
+              console.log(error);
+          }
+          console.log('response--------');
+          console.log(JSON.stringify(response));
+          console.log('body--------');
+          console.log(JSON.stringify(body));
+          console.log('error--------');
+          console.log(JSON.stringify(error));
+      });
 
-    // reply
-    conversation
-    .reply(`deploying version=${version}`)
-    .transition();
-    done();
-
+      // reply
+      conversation
+      .reply(`deploying app name=${appName} and version=${version}`)
+      .transition();
+      done();
+    } else {
+      // reply
+      conversation
+      .reply(`wrong param, app name=${appName} and version=${version}`)
+      .transition();
+      done();
+    }
   }
 };
